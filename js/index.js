b12f595cbf5003d51d0297681b791337f8251776
Sahil Goswami <145134217+sahilgoswami5180@users.noreply.github.com>
2023-10-25 22:06:17 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function Msize() {
+    document.getElementById('line-price').innerHTML = '₹550';
+}
+function Lsize() {
+    document.getElementById('line-price').innerHTML = '₹750';
+}
+function XLsize() {
+    document.getElementById('line-price').innerHTML = '₹888';
+}
+function XXLsize() {
+    document.getElementById('line-price').innerHTML = '₹999';
+}
